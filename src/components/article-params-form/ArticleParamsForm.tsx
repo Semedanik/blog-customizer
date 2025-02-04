62d73564e0efc5941f418f496fcecc9a2fb771a1
Danil Semenov <danil-semenov-2005@mail.ru>
2025-02-05 02:32:45 +0300

    first commit

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,140 @@
+import React, { useState, useRef } from 'react';
+import clsx from 'clsx';
+
+// Импорт компонентов UI
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
 
+// Импорт стилей
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+// Импорт констант и типов
+import {
+	fontFamilyOptions,
+	backgroundColors,
+	contentWidthArr,
+	ArticleStateType,
+	fontSizeOptions,
+	fontColors,
+	defaultArticleState,
+} from 'src/constants/articleProps';
+
+// Хук для закрытия при клике вне компонента
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+
+// Типизация пропсов компонента
+type ArticleParamsFormProps = {
+	params: ArticleStateType;
+	onParamsChange: (newParams: ArticleStateType) => void;
+};
+
+// Компонент формы параметров статьи
+export const ArticleParamsForm = ({
+	params,
+	onParamsChange,
+}: ArticleParamsFormProps) => {
+	const formRef = useRef<HTMLDivElement>(null);
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	// Состояния для хранения текущих значений параметров
+	const [bgColor, setBgColor] = useState(params.backgroundColor);
+	const [fontSize, setFontSize] = useState(params.fontSizeOption);
+	const [fontFamily, setFontFamily] = useState(params.fontFamilyOption);
+	const [width, setWidth] = useState(params.contentWidth);
+	const [color, setColor] = useState(params.fontColor);
+
+	// Обрабоитчик отправки формы
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onParamsChange({
+			...params,
+			backgroundColor: bgColor,
+			fontSizeOption: fontSize,
+			fontFamilyOption: fontFamily,
+			contentWidth: width,
+			fontColor: color,
+		});
+	};
+
+	// Обработчик сброса формы
+	const handleReset = () => {
+		onParamsChange(defaultArticleState);
+		setBgColor(defaultArticleState.backgroundColor);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setWidth(defaultArticleState.contentWidth);
+		setColor(defaultArticleState.fontColor);
+	};
+
+	//Для стилизации боковой панели
+	const sidebarClass = clsx(styles.container, {
+		[styles.container_open]: isSidebarOpen,
+	});
+
+	// Хук для закрытия боковой панели при клике вне её
+	useOutsideClickClose({
+		rootRef: formRef,
+		isOpen: isSidebarOpen,
+		onClose: () => setIsSidebarOpen(false),
+		onChange: () => {},
+	});
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={formRef}>
+			<ArrowButton
+				onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+				isOpen={isSidebarOpen}
+			/>
+			<aside className={sidebarClass}>
+				<form
+					className={styles.form}
+					onReset={handleReset}
+					onSubmit={handleSubmit}>
+					<Text size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						options={backgroundColors}
+						selected={bgColor}
+						onChange={setBgColor}
+						title='Цвет фона'
+					/>
+					<Select
+						options={fontFamilyOptions}
+						selected={fontFamily}
+						onChange={setFontFamily}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						options={fontSizeOptions}
+						selected={fontSize}
+						name='fontSize'
+						onChange={setFontSize}
+						title='Размер шрифта'
+					/>
+					<Select
+						options={fontColors}
+						selected={color}
+						onChange={setColor}
+						title='Цвет шрифта'
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={width}
+						onChange={setWidth}
+						title='Ширина контента'
+					/>
+					<Separator />
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button title='Сбросить' type='clear' />
+						<Button title='Применить' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
